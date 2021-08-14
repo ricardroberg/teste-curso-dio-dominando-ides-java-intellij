8f3af53e53bb2e3bcb979ab2b87e4e04a6174660
ricardroberg <ricardroberg@gmail.com>
2021-08-14 18:24:06 -0300

    Criado class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -11,9 +11,45 @@ public class PrimeiroPrograma {
         System.out.println("Este é " + gato.getNome() + "\nsua cor é " + gato.getCor() + ", bem bacana" +
                 "e ele já " + "tem " + gato.getIdade() + " anos.");
 
+        Livro livro1 = new Livro("O Problema dos 3 corpos", 300);
+        System.out.println(livro1);
+
 /*        int a = 5;
         int b = 3;
         System.out.println("Hello World! " + (a+b));*/
 
+    }
+}
+class Livro {
+        private String nome;
+        private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
     }
 }
